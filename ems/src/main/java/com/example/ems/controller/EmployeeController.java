900a3de9d83d8f7c69438300540f9867c99fb032
Trevor Karl <trevorkarl5@gmail.com>
2023-02-16 21:34:44 -0600

    Adding React Front-End Pages

--- ems/src/main/java/com/example/ems/controller/EmployeeController.java
+++ ems/src/main/java/com/example/ems/controller/EmployeeController.java
@@ -3,13 +3,17 @@ package com.example.ems.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ems.model.Employee;
 import com.example.ems.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -19,5 +23,10 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
